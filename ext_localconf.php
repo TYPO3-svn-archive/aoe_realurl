44266505404581c79f288868bd5d6574ba37dce1
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-02 14:10:28 +0000

    Keep patches up to date
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/aoe_realurl/tags/0.5.0@37592 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,20 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 include_once (t3lib_extMgm::extPath ('aoe_realurlpath') . 'class.tx_aoerealurlpath_pagepath.php');
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['aoe_realurlpath']);
+
 if ($confArr['applyPatch'] == 1) {
-	$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/realurl/class.tx_realurl.php'] = t3lib_extMgm::extPath('aoe_realurlpath') . 'patch/class.ux_tx_realurl.php';
+	function aoe_get_version_from_conf($_EXTKEY) {
+		include(t3lib_extMgm::extPath($_EXTKEY, 'ext_emconf.php'));
+		return substr($EM_CONF[$_EXTKEY]['version'].'       ', 0, strlen($EM_CONF[$_EXTKEY]['version']));
+	}
+
+	if (version_compare(aoe_get_version_from_conf('realurl'), '1.5.3', '<=')) {
+		$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/realurl/class.tx_realurl.php'] = t3lib_extMgm::extPath('aoe_realurlpath') . 'patch/1.5.3/class.ux_tx_realurl.php';
+	} elseif (version_compare(aoe_get_version_from_conf('realurl'), '1.9.4', '<=')) {
+		$GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/realurl/class.tx_realurl.php'] = t3lib_extMgm::extPath('aoe_realurlpath') . 'patch/1.9.4/class.ux_tx_realurl.php';
+	}
 }
+
 if ($confArr['addpageOverlayFields'] == 1) {
 	$GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_aoerealurlpath_overridesegment,tx_aoerealurlpath_overridepath,tx_aoerealurlpath_excludefrommiddle';
 }
