ff3700861538ffe8ae72f10ee9c5daa446d162f8
danp <danp@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-14 14:39:40 +0000

    add tab
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/aoe_realurl/trunk@43575 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -30,12 +30,12 @@ $tempColumns = array (
 
 t3lib_div::loadTCA("pages_language_overlay");
 t3lib_extMgm::addTCAcolumns('pages_language_overlay', $tempColumns, 1);
-t3lib_extMgm::addToAllTCAtypes('pages_language_overlay', 'tx_aoerealurlpath_overridepath;;;;1-1-1, tx_aoerealurlpath_excludefrommiddle,tx_aoerealurlpath_overridesegment,tx_aoerealurlpath_info', '', 'after:tx_realurl_pathsegment');
+t3lib_extMgm::addToAllTCAtypes('pages_language_overlay', '--div--;Url Settings,tx_aoerealurlpath_overridepath;;;;1-1-1, tx_aoerealurlpath_excludefrommiddle,tx_aoerealurlpath_overridesegment,tx_aoerealurlpath_info', '', 'after:tx_realurl_pathsegment');
 t3lib_extMgm::addToAllTCAtypes('pages_language_overlay', '--div--;RealUrl', '', 'before:tx_realurl_pathsegment');
 
 t3lib_div::loadTCA("pages");
 t3lib_extMgm::addTCAcolumns("pages", $tempColumns, 1 );
-t3lib_extMgm::addToAllTCAtypes("pages", "tx_aoerealurlpath_overridepath;;;;1-1-1, tx_aoerealurlpath_excludefrommiddle,tx_aoerealurlpath_overridesegment,tx_aoerealurlpath_info", '', 'after:tx_realurl_pathsegment');
+t3lib_extMgm::addToAllTCAtypes("pages", "--div--;Url Settings,tx_aoerealurlpath_overridepath;;;;1-1-1, tx_aoerealurlpath_excludefrommiddle,tx_aoerealurlpath_overridesegment,tx_aoerealurlpath_info", '', 'after:tx_realurl_pathsegment');
 t3lib_extMgm::addToAllTCAtypes('pages', '--div--;RealUrl', '', 'before:tx_realurl_pathsegment');
 
 $GLOBALS['TCA']['pages']['columns']['tx_realurl_pathsegment']['displayCond'] = 'EXT:aoe_realurlpath:LOADED:false';
